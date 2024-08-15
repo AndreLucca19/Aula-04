b52e47a7d9d085c47324b35ed8c3ee79323fdf7f
André Lucca <luccaandre78@gmail.com>
2024-08-15 09:16:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introdução: programação orientada a objetos
+
+Este repositório armazena exercicíos referente a programação orientada a objetas.
---
+++ aula-04.js
@@ -0,0 +1,76 @@
+//exercicio 01
+let livro = "O pequeno principe";
+
+const livroObjeto = new Object(livro)
+
+livroObjeto.titulo = "O pequeno principe"
+livroObjeto.autor = "Antoine de Saint-Exupéry"
+livroObjeto.ano = 1943
+
+console.log(livro);
+console.log(livroObjeto);
+
+//exercicio 02
+
+let aluno = "Zani";
+
+const alunoObjeto = new Object(aluno)
+
+alunoObjeto.nome = "Zani"
+alunoObjeto.turma = "2 ano"
+alunoObjeto.sala = 21
+
+console.log(aluno);
+console.log(alunoObjeto);
+
+//exercicio 03
+
+let carro = "O pequeno principe";
+
+const carroObjeto = new Object(carro)
+
+carroObjeto.cor = "Preto"
+carroObjeto.modelo = "Mclaren senna"
+carroObjeto.ano = 2017
+
+console.log(carro);
+console.log(carroObjeto);
+
+//exercicio 04
+
+let filme = "O pequeno principe";
+
+const filmeObjeto = new Object(filme)
+
+filmeObjeto.titulo = "O pequeno principe"
+filmeObjeto.diretor = "Mark Osborne"
+filmeObjeto.lancamento = 2015
+
+console.log(filme);
+console.log(filmeObjeto);
+
+//exercicio 05
+
+let cachorro = "O pequeno principe";
+
+const cachorroObjeto = new Object(cachorro)
+
+cachorroObjeto.nome = "Akira"
+cachorroObjeto.raca = "Kangal"
+cachorroObjeto.cor = "Branco"
+
+console.log(cachorro);
+console.log(cachorroObjeto);
+
+//exercicio 06
+
+let produto = "Doritos";
+
+const produtoObjeto = new Object(produto)
+
+produtoObjeto.descricao = "Um salgadinho muito bom"
+produtoObjeto.marca = "Doritos"
+produtoObjeto.preco = 11,99
+
+console.log(produto);
+console.log(produtoObjeto);
